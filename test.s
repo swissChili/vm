8f0a60f5a9cad7c87ce70bdea744d7b715acd2cf
swissChili <swisschili@fastmail.com>
2019-08-25 20:29:30 -0700

    Tried factorial script

--- test.s
+++ test.s
@@ -1,9 +1,37 @@
 @start
+	; Arg
 	PSH 8
 	POP EAX
 	CAL @fact-iter
 	END
 
 @fact-iter
-	PSH 123123
+	; Accumulator
+	PSH 1
+	POP EBX
+	; Iters
+	PSH 1
+	POP ECX
+
+	JMP @fact-iter:condition
+
+@fact-iter:iterate
+	LDR EBX
+	LDR ECX
+	MLT
+	POP EBX
+
+@fact-iter:condition
+	LDR ECX
+	LDR EAX
+	CMP
+	JEQ @fact-iter:done
+
+	LDR ECX
+	INC
+	POP ECX
+	JMP @fact-iter:iterate
+
+@fact-iter:done
+	LDR ECX
 	RET
